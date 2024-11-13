ddce6c5c0a6f1b1ff86b02d74e013e913f76e34a
Simlex <simlexafol@gmail.com>
2024-11-13 16:25:21 +0100

    refactor: clean up unused code and update TypeScript configuration

--- app/context/AuthContext.tsx
+++ app/context/AuthContext.tsx
@@ -2,9 +2,9 @@
 import React, { FunctionComponent, ReactNode } from 'react';
 import { SessionProvider } from "next-auth/react"
 
-interface AuthContextProps {
-  children: ReactNode | ReactNode[]
-}
+// interface AuthContextProps {
+//   children: ReactNode | ReactNode[]
+// }
 
 export const AuthProvider: FunctionComponent<{children: ReactNode}> = ({ children }) => {
 
