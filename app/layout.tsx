ac0e463855b3db6726829f6baf608b4e07f5dc27
Ayobami Ajayi <lynxdev32@gmail.com>
2024-11-19 19:35:48 +0100

    add general variables, typography, and basic page-routes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,24 @@
 import type { Metadata } from "next";
-import { Inter } from 'next/font/google'
+import { Inter } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
 export const metadata: Metadata = {
-    title: "Whipcare Admin Console",
-    description: "The console application for Whipcare",
+  title: "Whipcare Admin Console",
+  description: "The console application for Whipcare",
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="en">
-            <body
-                className={`${inter.className} antialiased`}
-            >
-                {children}
-            </body>
-        </html>
-    );
+  return (
+    <html lang='en'>
+      <body className={`${inter.variable} antialiased font-inter`}>
+        {children}
+      </body>
+    </html>
+  );
 }
