ee0b0889551c54ba057e1711583db2ad5dfb4631
onlyeugene <eugeneokogun@gmail.com>
2025-07-21 13:56:40 +0100

    feat: added the active and incative service providers

--- app/dashboard/feedbacks/layout.tsx
+++ app/dashboard/feedbacks/layout.tsx
@@ -1,14 +1,17 @@
-import { ReactNode } from 'react';
-import PageHeading from '../components/PageHeading';
-import FeeedbackNav from './FeeedbackNav';
+// import { ReactNode } from 'react';
+// import PageHeading from '../components/PageHeading';
+// import FeeedbackNav from './FeeedbackNav';
 
-const FeedBackLayout = ({ children }: { children: ReactNode }) => {
-  return (
-    <>
-      <PageHeading page='Feedbacks' pageFilters />
-      <FeeedbackNav />
-      {children}
-    </>
-  );
-};
+const FeedBackLayout = () =>
+  // { children }: { children: ReactNode }
+  {
+    return (
+      <>
+        {/* <PageHeading page='Feedbacks' pageFilters />
+      <FeeedbackNav /> */}
+        {/* {children} */}
+        Feedbacks
+      </>
+    );
+  };
 export default FeedBackLayout;
